89f245a22c089941a4f61d0071c6aa3271620831
Tony <tangvwin@gmail.com>
2017-03-19 23:37:56 +0800

    implement groups#destroy#show#edit#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,4 +9,51 @@ class GroupsController < ApplicationController
    end
 
 
+   def show
+     @group = Group.find(params[:id])
+   end
+
+
+
+    def create
+      @group = Group.new(group_params)
+      @group.save
+      redirect_to groups_path
+    end
+
+      def edit
+        @group = Group.find(params[:id])
+     end
+
+     def update
+       @group=Group.find(params[:id])
+       if @group.update(group_params)
+       redirect_to groups_path
+     else
+       render :edit
+     end
+   end
+
+
+
+
+    def destroy
+      @group = Group.find(params[:id])
+      @group.destroy
+      flash[:alert] = "Group delete!"
+   redirect_to groups_path
+ end
+
+
+
+
+
+    private
+
+    def group_params
+      params.require(:group).permit(:title, :description)
+    end
+
+
+
 end
