f9d6731beba93abbec55511701798ebcd78cfff4
Tony <tangvwin@gmail.com>
2017-03-22 17:11:50 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,14 @@ has_many :participated_groups, :through => :group_relationships, :source => :gro
    participated_groups.include?(group)
  end
 
+ def join!(group)
+   participated_groups << group
+ end
+
+ def quit!(group)
+   participated_groups.delete(group)
+ end
+
+
+
 end
